4f4d67e76fff62778d8ea7d16c699fc86defabf9
Jess MacQueen <jessmacqueen@gmail.com>
2018-04-04 15:19:12 -0700

    use object_type to pull object out of kwargs

--- src/sentry/api/endpoints/team_avatar.py
+++ src/sentry/api/endpoints/team_avatar.py
@@ -8,9 +8,3 @@ from sentry.models import TeamAvatar
 class TeamAvatarEndpoint(AvatarMixin, TeamEndpoint):
     object_type = 'team'
     model = TeamAvatar
-
-    def get(self, request, team):
-        return super(TeamAvatarEndpoint, self).get(request, team)
-
-    def put(self, request, team):
-        return super(TeamAvatarEndpoint, self).put(request, team)
